7d7699f59bd0023a12ff6bde36cf2280792c1057
Johannes Wieland <johanneswieland@MacBook-Pro-5.local>
2025-02-04 11:12:23 -0800

    graphmaker

---
+++ symlink_graph/code/graphmaker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Script to generate graph of tasks
+
+echo -e 'digraph G {' > ../output/graph.txt
+
+find ../../*/code -maxdepth 1 -name "makefile" | xargs grep -o 'input.*:.*output' |
+sed 's/\.\.\/\.\.\///g' | #Drop leading relative path ../../ from start of line
+sed 's/\/code\/makefile:input.*:/ \->/' | sed 's/\/output$//' | sed 's/ | / /' | #Drop folders and file names; show only tasks
+awk -F' -> ' '{ print $2 "->" $1}' | #Flip order to reflect task flow, not symbolic link direction
+sort | uniq | sed 's/^ //' >> ../output/graph.txt
+
+echo '}' >> ../output/graph.txt
