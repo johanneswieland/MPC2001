e4a665dd62855d3a9af6cf2ed937ae7f34018434
Jacob D Orchard <jake.orchard@frb.gov>
2025-03-19 10:03:50 -0400

    Update nk_rebates_mpc_plot2001.py
    Remove extra "The model is calibrated at a monthly frequency" line.

--- model/code/nk_rebates_mpc_plot2001.py
+++ model/code/nk_rebates_mpc_plot2001.py
@@ -363,7 +363,6 @@ for parasetname, parasettitle in zip(parasetnames, parasettitles):
                 f.write('\\\\ \n')
             f.write('\\bottomrule\\end{tabular}\n') 
             f.write('\\begin{minipage}{\hsize} \\rule{0pt}{9pt} \\footnotesize \n')
-            f.write('Notes: The model is calibrated at a monthly frequency. ')
             if 'gamma' in table.keys():
                 f.write('The model is calibrated at a monthly frequency. The parameter $\\gamma$ is calibrated to either 0.375, or 0.66, which corresponds to the 3-month and 6-month non-durable MPCs respectfully. See the text for details. ')
 
